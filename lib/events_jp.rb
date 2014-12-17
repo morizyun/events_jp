2648a11f30ff58023dcc256e21b1c08231a95835
morizyun <merii.ken@gmail.com>
2014-12-17 20:58:33 +0900

    add parallel get event function

--- lib/events_jp.rb
+++ lib/events_jp.rb
@@ -1,11 +1,15 @@
+require 'parallel'
+
 require 'events_jp/version'
 require 'events_jp/connection'
-require 'events_jp/event'
+
 require 'events_jp/event_object/event_object'
 require 'events_jp/event_object/atnd'
 require 'events_jp/event_object/connpass'
 require 'events_jp/event_object/doorkeeper'
 require 'events_jp/event_object/zusaar'
+
+require 'events_jp/event'
 
 module EventsJp
   extend EventsJp::Connection
--- lib/events_jp/event.rb
+++ lib/events_jp/event.rb
@@ -1,13 +1,17 @@
 module EventsJp
   module Event
     class InvalidException < Exception; end
+    SERVICES = [EventsJp::Atnd, EventsJp::Connpass, EventsJp::Doorkeeper, EventsJp::Zusaar]
 
     def get_events(keyword: nil, service_limit: nil)
-      obj = [EventsJp::Atnd, EventsJp::Connpass, EventsJp::Doorkeeper, EventsJp::Zusaar]
-      obj.map do |service|
+      results = []
+
+      Parallel.each(SERVICES, in_threads: SERVICES.count) do |service|
         next if service == EventsJp::Doorkeeper && keyword
-        service.get_events(keyword: keyword, limit: service_limit)
-      end.flatten.compact
+        results << service.get_events(keyword: keyword, limit: service_limit)
+      end
+
+      results.compact.flatten
     end
   end
 end
